f802efe2ab8ab13cc563dc590b80647f17d38504
lgaleana <lsgaleana@gmail.com>
2015-12-19 15:24:04 -0600

    Added app to apps list and made Experience model

--- app/models.py
+++ app/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Experience(models.Model):
+  name = models.CharField(max_length=50)
+  price = models.DecimalField(max_digits=6, decimal_places=2)
+  description = models.TextField()
+  location = models.CharField(max_length=100)
+  availability = models.CharField(max_length=100)
+  duration = models.DurationField(blank=True)
+  participants = models.IntegerField(default=1)
+  requirements = models.TextField(default="[]")
+  included = models.TextField(default="[]")
+  additional = models.TextField(blank=True)
