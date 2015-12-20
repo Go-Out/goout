fe53c1dc80ace9f555b9162972f3bfd242b3d484
lgaleana <lsgaleana@gmail.com>
2015-12-19 20:01:03 -0600

    Customized admin site

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Experience, Category, Demographic
+from datetime import timedelta
+from django import forms
 
 # Register your models here.
+
+class ExperienceForm(forms.ModelForm):
+  def __init__(self, *args, **kwargs):
+    super(ExperienceForm, self).__init__(*args, **kwargs)
+    if self.instance.duration is not None:
+      self.initial['duration'] = self.instance.duration.seconds / 3600
+      self.initial['requirements'] = json_as_text(self.instance.requirements)
+      self.initial['included'] = json_as_text(self.instance.included)
+      self.initial['additional'] = json_as_text(self.instance.additional)
+
+class ExperienceAdmin(admin.ModelAdmin):
+  list_display = ('name', 'price', 'location')
+  form = ExperienceForm
+
+  def save_model(self, request, obj, form, change):
+    obj.duration = timedelta(hours=obj.duration.seconds)
+    obj.requirements = text_as_json(obj.requirements)
+    obj.included = text_as_json(obj.included)
+    obj.additional = text_as_json(obj.additional)
+    obj.save()
+
+  def get_form(self, request, obj=None, **kwargs):
+    print "Hello!" 
+    return super(ExperienceAdmin, self).get_form(request, obj, **kwargs)
+
+def text_as_json(text):
+  elements = text.splitlines()
+  json = "[\"" + elements[0] + "\""
+  for i in range(1, len(elements)):
+    json += ",\"" + elements[i] + "\""
+  json += "]"
+  return json
+
+def json_as_text(json):
+  return json[2:len(json) - 2].replace("\",\"", "\n")
+
+admin.site.register(Experience, ExperienceAdmin)
+admin.site.register(Category)
+admin.site.register(Demographic)
--- app/models.py
+++ app/models.py
@@ -3,6 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.name
+
+class Demographic(models.Model):
+  name = models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.name
 
 class Experience(models.Model):
   name = models.CharField(max_length=50)
@@ -10,8 +21,13 @@ class Experience(models.Model):
   description = models.TextField()
   location = models.CharField(max_length=100)
   availability = models.CharField(max_length=100)
-  duration = models.DurationField(blank=True)
+  duration = models.DurationField(blank=True, help_text="Number of hours")
   participants = models.IntegerField(default=1)
-  requirements = models.TextField(default="[]")
-  included = models.TextField(default="[]")
-  additional = models.TextField(blank=True)
+  requirements = models.TextField(help_text="Each one in a new line")
+  included = models.TextField(help_text="Each one in a new line")
+  additional = models.TextField(help_text="Each one in a new line")
+  categories = models.ManyToManyField(Category)
+  demographics = models.ManyToManyField(Demographic)
+
+  def __str__(self):
+    return self.name
