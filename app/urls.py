eaae26fe01876cef2848ae0729a17b7b91464121
lgaleana <lsgaleana@gmail.com>
2015-12-21 15:12:42 -0600

    Created experiences list and experience detail

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [url(r'^$', views.index, name='index'),]
+urlpatterns = [
+  url(r'^$', views.index, name='index'),
+  url(r'^experiences/$', views.experiences, name='experiences'),
+  url(r'^experiences/(?P<experience_id>[0-9]+)/$', views.detail, name='detail'),
+]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,44 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import Experience, Category, Demographic
+from django.core import serializers
+import json
 
 # Create your views here.
 def index(request):
   return HttpResponse('Welcome to goout!')
+
+def experiences(request):
+  experiences = Experience.objects.all()
+  context = {'experiences': experiences}
+  return render(request, "app/experiences.html", context)
+
+def detail(request, experience_id):
+  experience_model = Experience.objects.get(pk=experience_id)
+  duration = experience_model.duration.seconds / 3600
+  requirements = json.loads(experience_model.requirements)
+  included = json.loads(experience_model.included)
+  additional = json.loads(experience_model.additional)
+
+  experience = serializers.serialize("python", [experience_model,])[0]["fields"]
+
+  experience["duration"] = duration
+  experience["requirements"] = requirements
+  experience["included"] = included
+  experience["additional"] = additional 
+
+  categories = []
+  category_pks = experience["categories"]
+  for category_pk in category_pks:
+    category = Category.objects.get(pk=category_pk).name
+    categories.append(category)
+  experience["categories"] = categories
+
+  demographics = []
+  demographic_pks = experience["demographics"]
+  for demographic_pk in demographic_pks:
+    demographic = Demographic.objects.get(pk=demographic_pk).name
+    demographics.append(demographic)
+  experience["demographics"] = demographics 
+
+  return JsonResponse(experience)
