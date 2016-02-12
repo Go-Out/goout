691de96bcde6750ca41dcbf1043c6b32070ad78e
lgaleana <lsgaleana@gmail.com>
2016-02-12 16:56:04 -0600

    Integrated payments sandbox

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from .models import Experience, Category
 from django.core import serializers
 import json
+import conekta
 
 # Create your views here.
 def index(request):
@@ -22,12 +23,57 @@ def json_detail(request, experience_id):
   return JsonResponse(experience_as_json(experience_model))
 
 def payment(request, experience_id):
-  return render(request, "app/payment.html", {})
+  return render(request, "app/payment.html", {'experience_id': experience_id})
 
+def process_payment(request, experience_id):
+  conekta.api_key = "key_fhT3iSqcYrPfsqpuggUZRQ"
+  charge = conekta.Charge.create({
+    "description":"Stogies",
+    "amount": 20000,
+    "currency":"MXN",
+    "reference_id":"9839-wolf_pack",
+    "card": "tok_test_visa_4242",
+    "details": {
+      "name": "Arnulfo Quimare",
+      "phone": "403-342-0642",
+      "email": "logan@x-men.org",
+      "customer": {
+        "logged_in": "true",
+        "successful_purchases": 14,
+        "created_at": 1379784950,
+        "updated_at": 1379784950,
+        "offline_payments": 4,
+        "score": 9
+      },
+      "line_items": [{
+        "name": "Box of Cohiba S1s",
+        "description": "Imported From Mex.",
+        "unit_price": 20000,
+        "quantity": 1,
+        "sku": "cohb_s1",
+        "category": "food"
+      }],
+      "billing_address": {
+        "street1":"77 Mystery Lane",
+        "street2": "Suite 124",
+        "street3": "null",
+        "city": "Darlington",
+        "state":"NJ",
+        "zip": "10192",
+        "country": "Mexico",
+        "tax_id": "xmn671212drx",
+        "company_name":"X-Men Inc.",
+        "phone": "77-777-7777",
+        "email": "purshasing@x-men.org"
+      }
+    }
+  })
+  return render(request, "app/payment_result.html", {'charge': charge})
 
 def experience_as_json(experience_model):
   experience = serializers.serialize("python", [experience_model,])[0]["fields"]
 
+  experience["id"] = experience_model.id
   experience["description"] = json.loads(experience_model.description)
   experience["price"] = float(experience_model.price)
   experience["duration"] = experience_model.duration.seconds / 3600
