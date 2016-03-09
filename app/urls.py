0790b2f0c915229257e74fe8c5adf6c688122e03
lgaleana <lsgaleana@gmail.com>
2016-03-09 12:18:25 -0600

    Extracted payment requests from views.py

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
   url(r'^experiences/json$', views.experiences_json, name='experiences_json'),
   url(r'^experiences/(?P<experience_id>[0-9]+)$', views.detail, name='detail'),
   url(r'^experiences/(?P<experience_id>[0-9]+)/json$', views.json_detail, name='json_detail'),
-  url(r'^experiences/(?P<experience_id>[0-9]+)/payment$', views.payment, name='payment'),
-  url(r'^process_payment/(?P<experience_id>[0-9]+)$', views.process_payment, name='process_payment'),
+#  url(r'^experiences/(?P<experience_id>[0-9]+)/payment$', views.payment, name='payment'),
+#  url(r'^process_payment/(?P<experience_id>[0-9]+)$', views.process_payment, name='process_payment'),
   url(r'^experiences/(?P<experience_id>[0-9]+)/availability$', views.experience_availability, name='experience_availability'),
 ]
