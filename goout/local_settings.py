e5bb8dfb2ac7a22df089634db0f2ebd9f428c064
lgaleana <lsgaleana@gmail.com>
2016-04-19 17:58:06 -0500

    Added local settings file

---
+++ goout/local_settings.py
@@ -0,0 +1,12 @@
+DEBUG = True
+
+DATABASES = {              
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'dbn3i9r2a5cgs2',     
+    'USER': 'udvzswapkwppai',     
+    'PASSWORD': '1j9zeA2gQ7sM9xF2-L8TeFcl6G',           
+    'HOST': 'ec2-54-163-247-73.compute-1.amazonaws.com',             
+    'PORT': '5432',                 
+  }
+}
