e70fde1846a5425d2bf9825141499b8d916b3cae
lgaleana <lsgaleana@gmail.com>
2016-04-25 20:06:26 -0500

    Implemented price function

--- app/static/app/javascripts/detail.js
+++ app/static/app/javascripts/detail.js
@@ -87,18 +87,23 @@
 
 
   var getPrice = function() {
-    var total = price * people;
-    return (total - (total * (0.73 * (people - 1) + 1.14 * (noExperiences - 1)) / 100)).toFixed(1);
+    if(experiencePrices.length > 0) {
+      var experiencesPrice = 0;
+      for(var i = 0; i < experiencePrices.length; i++)
+        experiencesPrice += experiencePrices[i] * people;
+      var total = getHuttPrice() + experiencesPrice;
+      return (total - (total * (0.73 * (people - 1) + 1.14 * experiencePrices.length) / 100)).toFixed(1);
+    }
+    else
+      return price;
   };
 
   var getHuttPrice = function() {
-    if(noExperiences < 1)
-      return 0;
     if(people < 5)
       return 1450;
     if(people < 9)
       return 2700;
-    if(people <= 10)
+    if(people < 11)
       return 2959;
     return 3900;
   };
